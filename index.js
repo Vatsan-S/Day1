7622fcb5f325ad3ab47098536f504ac96166245b
Vatsan-S <vatsan.designs@gmail.com>
2024-05-28 16:42:10 +0530

    completed

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000
+
+
+app.get("/",(req,res)=>{
+    // res.status(200).json({message:"Hi all welcome"})
+    res.status(200).send(`<div style="background-color:Aqua;color:black">Welcome to the page</div>`)
+})
+
+app.listen(PORT,()=>{
+    console.log(`We are listening  ${PORT}`)
+}) 
