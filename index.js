12501b90b0a95dcb3df6636595e1aba0aafa2399
Vatsan-S <vatsan.designs@gmail.com>
2024-05-28 16:49:40 +0530

    completed v2

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.get("/",(req,res)=>{
     // res.status(200).json({message:"Hi all welcome"})
     res.status(200).send(`<div style="background-color:Aqua;color:black">Welcome to the page</div>`)
 })
-
+app.get("/data",(req,res)=>{
+    res.status(200).send(`<div style="background-color:Aqua;color:black">Welcome to theeeeeeeeeee first page</div>`)
+})
 app.listen(PORT,()=>{
     console.log(`We are listening  ${PORT}`)
 }) 
